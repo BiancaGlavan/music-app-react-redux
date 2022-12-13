1ce00817584b3b900b348efd992850aa8d7f9330
BiancaGlavan <bianca.glavan@yahoo.ro>
2022-12-13 22:23:11 +0200

    updated queue content

--- src/components/music-player/QueueContent.tsx
+++ src/components/music-player/QueueContent.tsx
@@ -39,12 +39,14 @@ const QueueContent = (props: IPropsQueueContent) => {
     return (
         <StyledQueueContent>
             <Grid container spacing={2}>
-                <Grid item className="artist" xs={12} sm={4}>          
+                <Grid item xs={12} sm={4}>
+                    <Box className="artist">         
                     {playerState && <> <img className="image" src={playerState.activeSong?.album.cover_big || ''} alt="" />
                     <Typography className="title" variant="subtitle1">{playerState.activeSong?.title || ''}</Typography>
                     <Typography variant="caption">{playerState.activeSong?.artist.name}</Typography>
                     </>
-                    }               
+                    }
+                    </Box>                
                 </Grid>
                 <Grid item className="track-list" xs={12} sm={8}>
                 {playerState && <TrackList tracks={playerState.songList} cover={playerState.activeSong?.album.cover_medium || ''} />}
